cf28eb039320432a1615414f562dabbb1592f239
expl0itr <julian.goeritz@web.de>
2016-01-24 15:04:25 +0100

    Pushed SodiumLib & FirstAuth

---
+++ Ultrapowa Clash Server/PacketProcessing/Messages/Server/FirstAuthenticationOk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCS.Helpers;
+
+namespace UCS.PacketProcessing
+{
+    //Packet 20100
+    class FirstAuthenticationOk : Message
+    {
+        public FirstAuthenticationOk(Client client, FirstAuthentication cka)
+            : base(client)
+        {
+            SetMessageType(20100);
+        }
+
+        public static byte[] randomBytes = new byte[24];
+        public override void Encode()
+        {
+            // Randombytes()
+            Sodium.SodiumLibrary.randombytes_buff(randomBytes, 24);
+            List<Byte> pack = new List<Byte>();
+            pack.AddInt32(randomBytes[7]);  
+            pack.AddInt32(randomBytes[14]);
+            pack.AddInt32(randomBytes[21]);
+            SetData(pack.ToArray());
+        }
+    }
+}
+
